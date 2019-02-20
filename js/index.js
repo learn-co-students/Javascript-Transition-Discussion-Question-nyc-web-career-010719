c497ac429f5890ad20f86027ca07af5e9e27ebf1
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-20 12:05:53 -0500

    Done did it.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+let zombie = document.querySelector('#foreground');
+const save_lincoln = document.querySelector('#save_lincoln');
+
+save_lincoln.addEventListener("click", event => {
+  let time = document.querySelector('#interval').value;
+  myFade(time);
+}, false);
+
+function myFade(time) {
+  let op = 1;
+  debugger
+  var timer = setInterval(function ()
+    {
+    if (op <= 0.1) {
+      clearInterval(time);
+    }
+    zombie.style.opacity = op;
+    op -= 0.1;
+  }, time/10);
+};
