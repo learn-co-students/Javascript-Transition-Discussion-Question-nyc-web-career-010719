674fe75d614eeb8b1ec8edf06a0acb8d26168f93
github username <github email address>
2019-02-20 09:48:07 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+//----------------------------------------------------
+
+zombie = document.querySelector('#foreground')
+time = document.querySelector('#interval').value
+save_lincoln = document.querySelector('#save_lincoln')
+zombie.addEventListener("click", myFade, false)
+save_lincoln.addEventListener("click", event => {
+  myFade(time);
+}) //end of event listener
+
+
+
+function myFade(time){
+  let op = 1;
+  var timer = setInterval(function () {
+    if (op <= 0.1) {
+      clearInterval(time);
+    }
+    zombie.style.opacity = op;
+    op -= 0.1;
+  }, 50);
 }
+
+
+
+  // element.style.opacity(1, 0)
